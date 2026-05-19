67c1b071f68c7babec5d9525dfea50380c709f57
huunhantran <huunhantran0707@gmail.com>
2026-05-19 14:29:25 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/view/Dashboard.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Hotel Management System");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
